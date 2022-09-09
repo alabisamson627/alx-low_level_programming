8215ec4527a50ee4fd21a9bb144168f5cf6d85d7
alabi samson <alabisamson627@gmail.com>
2022-09-09 06:12:16 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - program prints with function puts
+ * main - program prints with function printf
  *
  * Return: Always 0
  */
 int main(void)
 {
-	printf("\ with proper grammar, but the outcome is a piece of art,);
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
