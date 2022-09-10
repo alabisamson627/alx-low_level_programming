af085f77081be2cedbb7066591777059b71b44ea
alabi samson <alabisamson627@gmail.com>
2022-09-10 07:48:36 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - lowercase alphabets
+ * main - print if the number is positive, zero, or negative
  *
- * Return: Always (Success)
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
  */
 int main(void)
 {
-	char c;
-
-	for (c ='a'; c <='z'; c++)
-		putchar(c)
-
-	putchar('\n');
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar( '\n' );
 	return (0);
 }
 
