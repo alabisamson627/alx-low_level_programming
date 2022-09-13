c249a1b0380a664a65937a4b78cc00d59e6915e9
alabi samson <alabisamson627@gmail.com>
2022-09-13 12:31:25 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,11 @@
 
 /**
  * main - print putchar
- * Return: always 0
+ * Return: Always 0
  */
 
 int main(void)
 {
-	
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -19,4 +18,3 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-
