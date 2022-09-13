94d0c51642e428a499ca556cd3d94ed547ff5a31
alabi samson <alabisamson627@gmail.com>
2022-09-13 11:13:23 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - if the character is _putchar
+ *
+ * Description - using the main function
+ * this program prints "alphabets"
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '_' ; ch = 'p' ; ch = 'u' ; ch = 't' ; ch = 'c' ; ch = 'h' ; ch = 'h' ; ch = 'a' ; ch = 'r')
+	{
+		printf("_purchar \n");
+	}
+	return (0);
+}
+
