f767c1763bfb28793c51ebae08ea649edc02a0a5
alabi samson <alabisamson627@gmail.com>
2022-09-13 23:11:05 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,11 @@
 int _isalpha(int c)
 {
 
-	(((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	{
 
 		return (1);
+	}
 
 	else
 		return (0);
