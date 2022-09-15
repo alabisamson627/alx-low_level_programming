686dad6c6b598d3409dac6d7d17505837da22da1
alabi samson <alabisamson627@gmail.com>
2022-09-15 00:41:06 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,21 +6,20 @@
  */
 
 int main(void)
+
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
+	printf("Infinite loog incoming :(\n");
 
 	i = 0;
-		/**
-		 * while (i < 10)
-		 * {
-		 * putchar(i);
-		 * }
-		 */
-
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
-
