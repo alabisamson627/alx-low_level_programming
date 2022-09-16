b8cae7d5c00106b993135ea48b454675c7a088fe
alabi samson <alabisamson627@gmail.com>
2022-09-16 07:01:28 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -35,7 +35,7 @@ void largest_prime_factor(long int num)
 {
 	int prmNu, largest;
 
-	/*first divide with the smallest prime number (two) */
+	/* first divide with the smallest prime number (two) */
 	while (num % 2 == 0)
 		num = num / 2;
 
@@ -43,10 +43,10 @@ void largest_prime_factor(long int num)
 	for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
 	{
 		while (num % prmNu == 0);
-				{
-				num = num / prmNu;
-				largest = prmNu;
-				}
+		{
+			num = num / prmNu;
+			largest = prmNu;
+		}
 	}
 
 	if (num > 2)
