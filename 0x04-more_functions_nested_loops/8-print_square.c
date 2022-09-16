ade908c001a0b6f284391eaf612566afbd668e1c
alabi samson <alabisamson627@gmail.com>
2022-09-16 06:00:15 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,23 +9,23 @@
 
 void print_square(int size)
 {
-	int i;
-	int j;
-	i = 0;
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		while (i < size)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			i++;
-			_putchar('\n');
-		}
-	}
+int i;
+int j;
+i = 0;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+while (i < size)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+i++;
+_putchar('\n');
+}
+}
 }
