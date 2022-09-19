b0def38dc3845ebd3c5bd382268efb87f19fdcbf
alabi samson <alabisamson627@gmail.com>
2022-09-19 14:12:34 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,10 +14,10 @@ int _atoi(char *s)
 	unsigned int num = 0;
 
 	do {
-		if (*s == '_')
+		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-		num = num * 10 + (*s - '0');
+			num = num * 10 + (*s - '0');
 		else if (num > 0)
 			break;
 	} while (*s++);
