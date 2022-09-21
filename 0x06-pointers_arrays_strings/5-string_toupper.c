f70dc04c1cc602c657227a8857d8a18aa50765b7
alabi samson <alabisamson627@gmail.com>
2022-09-21 13:07:51 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * atring_toupper - Changes all lowercase letters
+ * string_toupper - Changes all lowercase letters
  * of a string to uppercase.
  * @str: The string to be changed.
  *
