8f24dba690f195acd2ed6036c41c4103a6d75103
alabi samson <alabisamson627@gmail.com>
2022-09-21 13:20:33 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,7 +4,7 @@
  * leet - Encodes a string to 1337.
  * @str: The string to be encoded.
  *
- * Return: A pointer to the encode string.
+ * Return: A pointer to the encoded string.
  */
 
 char *leet(char *str)
@@ -22,7 +22,7 @@ char *leet(char *str)
 			       
 			       str[indx1]  = indx2 + '0';
 	       }
-
+	       
 	       indx1++;
        }
 
