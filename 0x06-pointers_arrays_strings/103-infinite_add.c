51c2ef6a80683bc5a26f12663b5bb27bb03ece7f
alabi samson <alabisamson627@gmail.com>
2022-09-22 00:08:50 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,4 @@
-#include "main.h"
-
+#include "main.h"+50
 
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
@@ -18,22 +17,23 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
 
-	for ( ; *n1 && *n2; n1--, n2--, r_index--)
+	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
+
 		num = (*n1 - '0') + (*n2 - '0');
 		num += tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
-	for ( ; *n1; n1--, r_index--)
+	for (; *n1; n1--, r_index--)
 	{
 		num = (*n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
-	for ( ; *n2; n2--, r_index--)
+	for (; *n2; n2--, r_index--)
 	{
 		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
@@ -41,7 +41,9 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	}
 
 	if (tens && r_index >= 0)
+
 	{
+
 		*(r + r_index) = (tens % 10) + '0';
 		return (r + r_index);
 	}
