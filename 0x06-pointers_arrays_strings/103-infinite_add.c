dabb7c72be6dba9653fbdd6d35bdc88844267190
alabi samson <alabisamson627@gmail.com>
2022-09-22 09:25:28 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,16 +18,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
-	
+
 	for (; *n1 && *n2; n1--, n2--, r_index--)
-	{	
+	{
 		num = (*n1 - '0') + (*n2 - '0');
 		num += tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 	for (; *n1; n1--, r_index--)
-	{	
+	{
 		num = (*n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
@@ -70,6 +70,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 	n1 += n1_len - 1;
 	n2 += n2_len - 1;
-	*(r + size_r) = '\0';	
+	*(r + size_r) = '\0';
 	return (add_strings(n1, n2, r, --size_r));
 }
