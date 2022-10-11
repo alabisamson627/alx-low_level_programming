fa97b8c068d60dfc80dd38162c709a93688f1341
alabi samson <alabisamson627@gmail.com>
2022-10-11 07:10:59 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,16 @@
 #include "dog.h"
 
 /**
- * init_dog - initializes a structure of type dog
+ * init_dog - intializes a structure of type dog
  * @d: pointer to structure
  * @name: pointer to the name of the dog
- * @age: age of dog
+ * @age: age of the dog
  * @owner: pointer to owner of dog
  *
  * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-
 {
 	if (d)
 	{
