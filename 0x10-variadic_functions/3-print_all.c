b697613dad50a0a42bef193e54eb2865f2d4c1d5
alabi samson <alabisamson627@gmail.com>
2022-10-14 06:55:41 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -38,7 +38,6 @@ void print_all(const char * const format, ...)
 				break;
 			case 'f':
 				printf("%f", va_arg(valist, double)), c = 1;
-
 				break;
 			case 's':
 				str = va_arg(valist, char *), c = 1;
