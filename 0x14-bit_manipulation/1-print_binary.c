941884b9e08fdb390365b0149d336089d8cf5e3c
alabi samson <alabisamson627@gmail.com>
2022-10-28 00:23:36 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,12 +2,13 @@
 
 /**
  * print_binary - prints the binary representation of a number
- * @n: unsgned long, number to be represented in binary
+ * @n: The number to be printed in binary
  */
 
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-	(n & 1) ? _putchar('1') : _putchar('0');
+	
+	_putchar((n & 1) + '0');
 }
