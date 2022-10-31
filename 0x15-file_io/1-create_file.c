ea585bf521c918111d0dc0dbf816a15b7ef60663
alabi samson <alabisamson627@gmail.com>
2022-10-31 11:16:44 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the fails.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content);
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		test_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
