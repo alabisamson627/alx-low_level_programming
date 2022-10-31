2c2d4cd22f7a2866e6b6da6003a8f520274288a1
alabi samson <alabisamson627@gmail.com>
2022-10-31 12:51:51 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
-		test_content = "";
+		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
